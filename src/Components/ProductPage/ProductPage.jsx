df6ab484a016bbeb09152c8feddb503faa2c96c5
Raviprasaath <raviprasaath1@gmail.com>
2023-10-16 07:56:59 +0530

    10-16-2023

--- src/Components/ProductPage/ProductPage.jsx
+++ src/Components/ProductPage/ProductPage.jsx
@@ -7,14 +7,17 @@ import img5 from "../../assets/categories-for-women/5.jpg"
 import img6 from "../../assets/categories-for-women/6.jpg"
 
 import { AiFillStar } from "react-icons/ai"
+import { GiPriceTag } from "react-icons/gi"
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper/modules";
+import { Label } from "@radix-ui/react-menubar";
 
 const ProductPage = () => {
-  const imageContainer = (
+
+    const imageContainer = (
     <>
       <Swiper
         spaceBetween={30}
@@ -58,34 +61,89 @@ const ProductPage = () => {
 
       </Swiper>
     </>
-  );
+    );
+
+    const productPriceDescription = (
+        <div className="px-2">
+            <p className="text-[0.9rem] font-bold px-2">Product name</p>
+            <p className="text-[0.8rem] opacity-70 px-2">Category</p>
+            <p className="px-2">
+                <span className="text-[1rem] font-bold">                    
+                    ₹ 799 
+                </span>
+                <span className="line-through text-[0.8rem] px-1 opacity-70">
+                    ₹ 1598 
+                </span>
+                <span className="px-1 text-[0.9rem] text-green-500 font-bold">
+                    (50% Off)
+                </span>
+            </p>
+            <p className="text-[0.8rem] opacity-50 px-2">Inclusive of Taxes + Free Shipping</p>
+            <div className="flex ">
+                <GiPriceTag className="text-rose-500 mx-1 text-[1.1rem]"/> 
+                <p className="text-[0.7rem] font-bold">
+                    Extra 100 OFF on ₹999 (Code: BEYOUNG100)
+                </p>
+            </div>
+        </div>
+    )
+
+    const sizeSelection = (
+        <div className="flex items-center gap-2.5 justify-center" >
+                <div className="text-[0.75rem] rounded-full border-2 w-[30px] pt-[3.2px] h-[30px] text-center">
+                    S
+                </div>
+                <div className="text-[0.75rem] rounded-full border-2 w-[30px] pt-[3.2px] h-[30px] text-center">
+                    M
+                </div>
+                <div className="text-[0.75rem] rounded-full border-2 w-[30px] pt-[3.2px] h-[30px] text-center">
+                    L
+                </div>
+                <div className="text-[0.75rem] rounded-full border-2 w-[30px] pt-[3.2px] h-[30px] text-center">
+                    XL
+                </div>
+                <div className="text-[0.75rem] rounded-full border-2 w-[30px] pt-[3.2px] h-[30px] text-center">
+                    XXL
+                </div>
+            </div>
+    )
+    const quantity = (
+        <div className="flex px-2">
+            <Label htmlFor="qty">QTY: </Label>
+            <select className="mx-2 px-1 w-[70px] border-2" name="qty" id="qty">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+            </select>                
+        </div>
+    )
+    
 
   return (
     <>
         {imageContainer}
         <div>
-            <p>Product name</p>
-            <p>Category</p>
-            <p>
-                <span className="px-2 text-[1.2rem] font-bold">                    
-                    ₹ 799 
-                </span>
-                <span className="line-through px-2">
-                    ₹ 1598 
-                </span>
-                <span className="px-2 font-bold">
-                    (50% Off)
-                </span>
-            </p>
-            <p>Inclusive of Taxes + Free Shipping</p>
-            <div className="flex ">                
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                <AiFillStar />
-                
+            {productPriceDescription}
+
+            <div className="my-2 border-2"></div>
+            {sizeSelection}
+            <div className="my-2 border-2"></div>
+            {quantity}
+            <div className="my-2 border-2"></div>
+            <div>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore cupiditate aut possimus, ea optio asperiores, fugit numquam beatae suscipit eos, impedit sint laboriosam ipsam? Consectetur eum veniam qui debitis quaerat!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore cupiditate aut possimus, ea optio asperiores, fugit numquam beatae suscipit eos, impedit sint laboriosam ipsam? Consectetur eum veniam qui debitis quaerat!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore cupiditate aut possimus, ea optio asperiores, fugit numquam beatae suscipit eos, impedit sint laboriosam ipsam? Consectetur eum veniam qui debitis quaerat!
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolore cupiditate aut possimus, ea optio asperiores, fugit numquam beatae suscipit eos, impedit sint laboriosam ipsam? Consectetur eum veniam qui debitis quaerat!
             </div>
+
         </div>
     </>
   );
